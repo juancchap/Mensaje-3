a0d1eb47c0d66d098678f2375ced05ac67064e6c
juancchap <156474242+juancchap@users.noreply.github.com>
2024-01-15 08:36:57 -0600

    Java Primer ejercicio

--- src/mensaje3/Mensaje3.java
+++ src/mensaje3/Mensaje3.java
@@ -15,7 +15,7 @@ public class Mensaje3 {
      */
     public static void main(String[] args) {
         int a=10;
-        System.out.println(a);
+        System.out.println("El valor es:"+a);
         
     }
     
