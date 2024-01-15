54c96f6e8dc878857d39047554313a8a4cdfe707
juancchap <156474242+juancchap@users.noreply.github.com>
2024-01-15 08:28:07 -0600

    Java Primer ejercicio

---
+++ src/mensaje3/Mensaje3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mensaje3;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Mensaje3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        
+    }
+    
+}
